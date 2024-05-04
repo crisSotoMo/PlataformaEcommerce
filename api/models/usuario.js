1818b5617b51da0c8aeca7d54c8c0f95a19e93d3
Arturo Chaves <artch92@gmail.com>
2024-05-03 18:02:38 -0600

    Se agregaron cambios de Chris

--- api/models/usuario.js
+++ api/models/usuario.js
@@ -1,20 +1,20 @@
-
 const mongoose = require("mongoose");
 
+const options = { discriminatorKey: 'tipoUsuario' };
+
 const schema_usuario = new mongoose.Schema({
-    rol: {type: String, required: true, unique: false}, // Se agrego rol como atributo de Usuario
-    tipoIdentificacion: {type: String, required: true, unique: false},
-    identificacion: {type: String, required: true, unique: true}, // Se cambio nombre de cedula por identificacion como atributo de Usuario
+    rol: {type: String, required: true, unique: false},
+    foto: {type: String, required: false, unique: false},
     nombre: {type: String, required: true, unique: false},
     apellido: {type: String, required: true, unique: false},
-    telefono: {type: String, required: true, unique: false},
+    tipoIdentificacion: {type: String, required: true, unique: false},
+    identificacion: {type: String, required: true, unique: true},
     correo: {type: String, required: true, unique: true},
-    contrasenna: {type: String, required: true, unique: false},
-    foto: {type: String, required: false, unique: false},
-    estado: {type: String, default: "Inactivo"}
-});
+    telefono: {type: String, required: true, unique: false},
+    contrasena: {type: String, required: true, unique: false},
+    estado: {type: String, default: "Inactivo"},
+}, options);
 
-module.exports = mongoose.model("Usuario", schema_usuario, "usuario");
+const Usuario = mongoose.model("Usuario", schema_usuario, "usuario");
 
-// cedula se agarra como string??
-// tipo de identificación dos opciones.  
+module.exports = Usuario;
--- api/routes/usuario.js
+++ api/routes/usuario.js
@@ -1,73 +1,62 @@
 const express = require("express");
 const Usuario = require("../models/usuario");
+const usuario_vendedor = require("../models/usuarioVendedor");
 const router = express.Router();
 
 // POST
-// URL: http://localhost:8000/api/usuario/ingresar
-router.post("/ingresar",function (req, res){
-    // logica = si la informacion ingresada por un usuario, tiene la misma contrasenna y el mismo email en una lista, debe de aceptar la info, de lo contrario debe mostrar que los datos están incorrectos. 
+router.post("/ingresar", function (req, res) {
+    // logica = si la informacion ingresada por un usuario, tiene la misma contrasena y el mismo email en una lista, debe de aceptar la info, de lo contrario debe mostrar que los datos están incorrectos.
     let correo = req.body.correo;
-    let contrasenna = req.body.contrasenna;
+    let contrasena = req.body.contrasena;
     Usuario.find({
         correo
     })
-    .then((buscarUsuario) => {
-        if(buscarUsuario[0].contrasenna == contrasenna){
-            res.status(200).json({
-                mensaje: "¡Inicio de sesión exitoso!",
-                resultado: buscarUsuario
-            })
-        }
-        else{
-            res.status(200).json({
-                mensaje: "Correo o contraseña invalido",
-                resultado: false
-            })
-        }
-    })
-    .catch((error) => {
-        res.json({
-            resultado: false,
-            error,
+        .then((buscarUsuario) => {
+            if (buscarUsuario[0].contrasena == contrasena) {
+                res.status(200).json({
+                    mensaje: "Inicio de sesión exitoso!",
+                    resultado: buscarUsuario
+                })
+            }
+            else {
+                res.status(200).json({
+                    mensaje: "Correo o contraseña invalido",
+                    resultado: false
+                })
+            }
         })
-    })
+        .catch((error) => {
+            res.json({
+                mensaje: "Error de logueo",
+                resultado: false,
+                error,
+            })
+        })
 }
 )
+router.post('/register', async (req, res)=> {
+    try{
+        const newUser = new Usuario(req.body);
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch(error){
+        console.error(error);
+        res.status(500).json({message: 'Hubo un error al crear el suaurio'})
+    }
+});
 
-// POST
-// URL: http://localhost:8000/api/usuario/registrar
-router.post("/registrar", function (req, res) {
-    let body = req.body;
-    let nuevo_usuario = new Usuario({
-        rol: body.rol, // Se agrego rol como atributo de Usuario
-        tipoIdentificacion: body.tipoIdentificacion,
-        identificacion: body.identificacion, // Se cambio nombre de cedula por identificacion como atributo de Usuario
-        nombre: body.nombre,
-        apellido: body.apellido,
-        telefono: body.telefono,
-        correo: body.correo,
-        contrasenna: body.contrasenna,
-        foto: body.foto,
-    });
+router.post('/register_seller', async (req, res)=> {
+    try{
+        const newUser = new usuario_vendedor(req.body);
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch(error){
+        console.error(error);
+        res.status(500).json({message: 'Hubo un error al crear el suaurio'})
+    }
+});
 
-    // error, usuarioDB
-    nuevo_usuario.save()
-        .then((usuarioDB) => {
-            res.status(201).json({
-                mensaje: "Usuario registrado",
-                resultado: true,
-                usuarioDB
-            })
-        }).catch((error) => {
-            res.status(501).json({
-                mensaje: "Error registrando usuario",
-                resultado: false,
-            })
-        })
-})
-
-// GET GENERAL 
-// URL: http://localhost:8000/api/usuario/listar
+// GET LISTAR USUARIOS
 router.get('/listar', function (req, res) {
     Usuario.find()
         .then((listaUsuarios) => {
@@ -83,28 +72,63 @@ router.get('/listar', function (req, res) {
         })
 })
 
-// GET USUARIO POR IDENTIFICACION
-// URL: http://localhost:8000/api/usuario/buscar-persona-identificacion/:id
-router.get("/buscar-persona-identificacion/:id", (req, res) => {
-    Usuario.find({
-            identificacion: req.params.id
+// GET USUARIO POR CEDULA
+router.get("/buscar-usuario-identificacion", (req, res) => {
+    Usuario.find({ identificacion: req.query.identificacion }).exec()
+        .then(usuarioBuscado => {
+            if (usuarioBuscado.length === 0) {
+                res.json({
+                    msj: "El usuario no existe",
+                });
+            } else {
+                res.status(200).json({
+                    resultado: true,
+                    usuarioBuscado
+                });
+            }
         })
-        .then((buscarIdentificacion) => {
-            res.status(200).json({
-                mensaje: "Identificación Encontrada",
-                resultado: buscarIdentificacion 
-            })
-        })
-        .catch((error) => {
-            res.json({
-                resultado: false,
-                error,
-            })
-        })
-})
+        .catch(error => {
+            res.status(501).json({
+                msj: "Ocurrió el siguiente error",
+                error
+            });
+        });
+});
+
+// GET USUARIO POR ID
+router.get('/usuario/:userId/productos/:productId', function (req, res) {
+    const usuarioId = req.params.userId;
+    const productoId = req.params.productId;
+
+    Usuario.findById(usuarioId)
+        .then((usuario) => {
+            if (!usuario) {
+                res.status(404).json({
+                    mensaje: "El usuario no fue encontrado"
+                });
+            } else {
+                const producto = usuario.productos.find(producto => producto._id.toString() === productoId);
+                if (!producto) {
+                    res.status(404).json({
+                        mensaje: "El producto no fue encontrado para este usuario"
+                    });
+                } else {
+                    res.status(200).json({
+                        mensaje: "Producto encontrado",
+                        producto: producto
+                    });
+                }
+            }
+        }).catch((error) => {
+            res.status(500).json({
+                mensaje: "Ocurrió un error al buscar el producto",
+                error: error
+            });
+        });
+});
+
 
 // Actualizar información de Usuario
-// URL: http://localhost:8000/api/usuario/modificar
 router.put("/modificar", (req, res) => {
     const {
         _id,
@@ -112,10 +136,10 @@ router.put("/modificar", (req, res) => {
     } = req.body;
 
     Usuario.updateOne({
-            _id
-        }, {
-            $set: updateData
-        })
+        _id
+    }, {
+        $set: updateData
+    })
         .then((info) => {
             res.status(200).json({
                 resultado: true,
@@ -133,15 +157,15 @@ router.put("/modificar", (req, res) => {
 })
 
 // ELiminar Usuario
-// URL: http://localhost:8000/api/usuario/eliminar
-router.delete("/eliminar", async (req, res) => {
+
+//DELETE http://localhost:3000/api/usuario/eliminar/123456
+
+router.delete("/eliminar/:id", async (req, res) => {
     try {
-        const {
-            _id
-        } = req.body;
+        const { id } = req.params;
 
         const info = await Usuario.deleteOne({
-            _id
+            _id: id
         });
 
         if (info.deletedCount === 0) {
@@ -166,3 +190,36 @@ router.delete("/eliminar", async (req, res) => {
 });
 
 module.exports = router
+
+
+//AGREGAR PRODUCTOS
+
+//localhost:3000/api/usuario/agregar_productos
+router.post("/agregar_productos", (req, res) => {
+    if (req.body._id) {
+        Usuario.updateOne({ _id: req.body._id }, {
+            $push: {
+                'productos': {
+                    nombre_prod: req.body.name,
+                    descripcion: req.body.descripcion
+                }
+            }
+        }).then(()=>{
+            res.status(201).json({
+                resultado: true,
+                msj: "Producto agregado",
+            });
+        }).catch((error) => {
+            res.status(501).json({
+                resultado: false,
+                msj: "No se pudo agregar el producto debido al error:",
+                error: error
+            });
+        })
+    } else {
+        res.status(501).json({
+            resultado: false,
+            msj: "No se proporcionó un ID",
+        });
+    }
+})
